bbb909daeeabb469e32225588729d2f604dd6b1f
Mukunth-arya <mukunthnataraj@gmail.com>
2022-02-21 20:15:57 +0530

    version8

--- main.go
+++ main.go
@@ -7,13 +7,20 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const connection = "mongodb+srv://mukunth:mukunth@mycluster.jptcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const dbname = "value"
-const colname = "reviewvalue"
+const newvalue = "value"
+const oldvalue = "reviewvalue"
+
+type datafunction struct {
+	ID   primitive.ObjectID `bson: "_id, omitempty"`
+	name string             `bson: "name,omitempty"`
+	age  string             `bson: age, omitempty`
+}
 
 func main() {
 
@@ -28,42 +35,111 @@ func main() {
 
 		log.Fatal(err)
 	}
+	database1 := data1.Database(newvalue)
+	data2 := database1.Collection(oldvalue)
 
-	dataconnect := data1.Database(dbname)
-	data2 := dataconnect.Collection(colname)
-	println("hey there the database is created", dataconnect)
-	//var datavalue bson.M
-	//if err = data2.FindOne(ctx, bson.M{}).Decode(&datavalue); err != nil {
+	//dat,3, err := data2.InsertOne(ctx, bson.D{
+	//{"name", "arya"},
+	//{"age", "21"},
+	//})
+	//if err != nil {
 
 	//log.Fatal(err)
 	//}
-	//fmt.Println(datavalue)
-	//var data []bson.M
-	//if err = data5.All(ctx, &data); err != nil {
+	//fmt.Println("Here comes the value", data3.InsertedID)
+
+	//data4, err := data2.InsertMany(ctx, []interface{}{
+	//bson.D{
+	//{"name", "kumar"},
+	//{"age", "23"},
+	//},
+	//bson.D{
+
+	//{"name", "vaibhav"},
+	//{"age", "40"},
+	//},
+	//})
+	//if err != nil {
 	//log.Fatal(err)
 	//}
-	//for _, data2 := range data {
-	//fmt.Println(data2)
+	//fmt.Println("here we added the more documents", data4.InsertedIDs)
+	//data3, err := data2.Find(ctx, bson.D{})
+	//if err != nil {
+	//log.Fatal(err)
+
 	//}
-	//defer data5.Close(ctx)
-	//for data5.Next(ctx) {
+	//for data3.Next(ctx) {
+	//var file []bson.M
+	//if err = data3.All(ctx, &file); err != nil {
+
+	//log.Fatal(err)
+	//}
+
+	//fmt.Println(file)
+
+	//}
 	//var data3 bson.M
-	//if err = data5.Decode(&data3); err != nil {
+	//if err = data2.FindOne(ctx, bson.M{}).Decode(&data3); err != nil {
 
 	//log.Fatal(err)
 	//}
-	//fmt.Println(data3)
-
+	//fmt.Println("here comes the data", data3)
+	//data3, err := data2.Find(ctx, bson.M{"name": "arya"})
+	//if err != nil {
+	//log.Fatal(err)
 	//}
-	datakey, err := data2.Find(ctx, bson.M{"name": "mukunth"})
+
+	//var dataone []bson.M
+
+	//if err = data3.All(ctx, &dataone); err != nil {
+	//log.Fatal(err)
+	//}
+	//fmt.Print(dataone)
+	//data4, _ := primitive.ObjectIDFromHex("62125d2836cb6651a3758090")
+	//if err != nil{
+	//log.Fatal(err)
+	//}
+	//data3, err := data2.UpdateOne(
+	//ctx,
+	//bson.M{"_id": data4},
+	//bson.D{
+	//{"$set", bson.D{{"name", "kumarboy"}}},
+	//},
+	//)
+
+	//if err != nil {
+	//log.Fatal(err)
+	//}
+	//fmt.Println("datamodifed count", data3.ModifiedCount)
+
+	//data6, err := data2.UpdateMany(
+	//ctx,
+	//bson.M{"age": "21"},
+	//bson.D{
+	//{"$set", bson.D{{"name", "periyasamy"}}},
+	//},
+	//)
+	//if err != nil {
+
+	//log.Fatal(err)
+	//}
+	//fmt.Println(data6.ModifiedCount)
+	//data6,err := data2.ReplaceOne(
+	//ctx,
+	//)
+	//data7, err := data2.ReplaceOne(
+
+	//ctx,
+	//bson.M{"name": "arya"},
+	//bson.M{
+	//"name": "aryahome",
+	//"age":  "21",
+	//},
+	//)
+	//fmt.Println(data7.ModifiedCount)
+	datafield, err := data2.DeleteOne(ctx, bson.M{"name": "kumar"})
 	if err != nil {
 		log.Fatal(err)
 	}
-	var keyvalue []bson.M
-	if err = datakey.All(ctx, &keyvalue); err != nil {
-		log.Fatal(err)
-	}
-	for _, value := range keyvalue {
-		fmt.Println(value)
-	}
+	fmt.Println(datafield.DeletedCount)
 }
